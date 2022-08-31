aed39d26e5dc1af1d5ef48548ed263fadf5839a2
veronica_rocha <veronica.rocha@dasa.com.br>
2022-08-30 21:46:26 -0300

    feat: adjustment docker

--- src/infra/database.providers.ts
+++ src/infra/database.providers.ts
@@ -1,21 +1,7 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import {config} from 'dotenv'
-
+import { DataSource } from 'typeorm';
 config()
 
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'mariadb',
-//   host:process.env.MYSQL_HOST,
-//   port: 3306,
-//   username: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DB_NAME,
-//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-//   synchronize: true,
-//   logging: true
-// };
-
-import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
   {
